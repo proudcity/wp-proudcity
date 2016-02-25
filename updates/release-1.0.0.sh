2c6655f599572bf427e2cb88a5e5ac2290f20174
Jeff Lyon <jeff@albatrossdigital.com>
2016-02-25 10:41:02 -0800

    Adding packages

--- updates/release-1.0.0.sh
+++ updates/release-1.0.0.sh
@@ -1,5 +1,5 @@
 # Updates for the 1.0.0 release
 
 wp option set validation_metatags '' --allow-root
-wp plugin install w3-total-cache wpmandrill mce-table-buttons black-studio-tinymce-widget --allow-root
-wp plugin activate gravityforms gravityformsmailchimp gravity-forms-bootstrap-3-style disable-comments wp-proud-document google-analytics-dashboard-for-wp so-widgets-bundle --allow-root
+wp plugin install w3-total-cache wpmandrill mce-table-buttons --allow-root
+wp plugin activate gravityforms gravityformsmailchimp gravity-forms-bootstrap-3-style disable-comments wp-proud-document google-analytics-dashboard-for-wp iframe so-widgets-bundle safe-redirect-manager --allow-root
