44b14c98ddb5ac1c0287f8d4cc1526ba42a5992b
Jeff Lyon <jeff@albatrossdigital.com>
2016-02-25 10:29:14 -0800

    Updating release 1.0.0 script

--- updates/release-1.0.0.sh
+++ updates/release-1.0.0.sh
@@ -2,3 +2,4 @@
 
 wp option set validation_metatags '' --allow-root
 wp plugin install w3-total-cache wpmandrill mce-table-buttons black-studio-tinymce-widget --allow-root
+wp plugin activate gravityforms gravityformsmailchimp gravity-forms-bootstrap-3-style disable-comments wp-proud-document google-analytics-dashboard-for-wp so-widgets-bundle --allow-root
