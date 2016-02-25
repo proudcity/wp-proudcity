a302a8eae002c8bcd676295e79974cef73cd043a
Jeff Lyon <jeff@albatrossdigital.com>
2016-02-24 16:31:08 -0800

    Adding updates

---
+++ updates/release-1.0.0.sh
@@ -0,0 +1,4 @@
+# Updates for the 1.0.0 release
+
+wp option set validation_metatags '' --allow-root
+wp plugin install w3-total-cache wpmandrill mce-table-buttons black-studio-tinymce-widget --allow-root
