fd756bbc0385055e78d6a7232c0fce5e70f33b0d
Jeff Lyon <jeff@albatrossdigital.com>
2016-04-19 20:50:45 -0700

    Adding Release 1.4.0 bash scripts

---
+++ updates/release-1.4.0.php
@@ -0,0 +1,10 @@
+<?php
+
+$opt = get_option('sidebars_widgets');
+$opt['sidebar-footer'] = array (
+  0 => 'proud_contact_block-2',
+  1 => 'gform_widget-2',
+  2 => 'proud_social_links-2',
+);
+update_option('sidebar-footer', $opt);
+print_r($opt);
---
+++ updates/release-1.4.0.sh
@@ -0,0 +1,7 @@
+# Updates for the 1.4.0 release
+# Update footer action widgets
+
+PHP=${1}
+
+# Update footer action widgets
+wp --allow-root eval-file $PHP
