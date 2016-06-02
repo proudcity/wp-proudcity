bfcd1e770325627867367263b4522685bd343672
Jeff Lyon <jeff@albatrossdigital.com>
2016-06-02 00:59:13 -0700

    Adding wp-mail-smtp settings to release-1.7.0.sh #395

--- updates/release-1.7.0.sh
+++ updates/release-1.7.0.sh
@@ -1,9 +1,22 @@
-# Updates for the 1.5.0 release
-# (no manual updates)
+# Updates for the 1.7.0 release
+# Update admin_email
+# Deactivate wp-ses
+# Activate wp-mail-smtp
+# Send test email
 
 PHP=${1}
-wp --allow-root option set admin_email "notify@proudcity.com"
 
-wp eval "
+# Update admin_email
+wp --allow-root option update admin_email "notify@proudcity.com"
+
+# Deactivate wp-ses
+wp --allow-root plugin deactivate wp-ses
+
+# Activate wp-mail-smtp
+wp --allow-root plugin install --activate wp-mail-smtp
+
+
+# Send test email
+wp --allow-root eval "
 wp_mail ( 'jeff@proudcity.com', 'Testing wp_mail', 'Testing wp_mail' );
 "
