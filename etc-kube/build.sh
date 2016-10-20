5860db3672b9ef4aa271802ee02cc676a2f56dfe
Jeff Lyon <jeff@albatrossdigital.com>
2016-10-19 20:46:12 -0700

    Continuing config

--- etc-kube/build.sh
+++ etc-kube/build.sh
@@ -1,12 +1,15 @@
 # SSL One-time setup:
+#source etc-kube/globals.sh 
 #kubectl create secret generic tls --from-file=combined.crt --from-file=proudcity.com.key --namespace jenkins
-#kubectl create ns $NAMESPACE
-#kubectl create --namespace $NAMESPACE -f etc-kube/secrets-proudcity.yml
+#kubectl create ns $NAMESPACE && kubectl create --namespace $NAMESPACE -f etc-kube/secrets-proudcity.yml
 
 
 # Globals
-bash globals.sh
+thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $thisdir/globals.sh
 
+echo "NAMESPACE set to $NAMESPACE"
+echo $thisdir
 # Arguments
 state=OR
 city="Corn Valley"
@@ -16,23 +19,25 @@ state="${state,,}"
 city="${city,,}"
 
 # Optional arguments?
-glue="_"
-key=`echo ${city// /_}$glue$state`
+glue=""
+key="${city// /}$glue$state"
+key=beta
 glue="-"
 suffix=".proudcity.com"
-host=`echo ${key//_/-}$glue$state$suffix`
+host="${city// /-}$glue$state$suffix"
 
 # Variables setup
-dbname=`echo $key | cut -c 1-16`
+dbname=`echo ${key//-/_} | cut -c 1-16`
 dbpass=`date +%s | sha256sum | base64 | head -c 32 ; echo`
 
 # Copy the Kube config
 if [ ! -d ../builds ]; then mkdir ../builds; fi
-dir="../builds/${key}"
+dir="$thisdir/../builds/${key}"
 echo "BUILD DIR: ${dir}"
 if [ -d $dir ]; then rm -Rf $dir; fi
-cp -r ../etc-kube $dir
+cp -r "$thisdir/../etc-kube" $dir
 cd $dir
+
 
 # Mysql setup
 echo "
@@ -40,7 +45,6 @@ CREATE database ${dbname};
 GRANT ALL ON ${dbname}.* TO ${dbname}@'%' IDENTIFIED BY '${dbpass}';
 " > create.sql
 mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} -P${MYSQL_PORT} < create.sql
-
 
 # Create our secrets file
 echo "
@@ -64,18 +68,20 @@ data:
   logged-in-salt: `base64 --wrap=0 /dev/urandom |head -c 64`
   nonce-salt: `base64 --wrap=0 /dev/urandom |head -c 64`
   gcserviceaccount.json: bm90aGluZw==
-" > secrets.yml
+" > $dir/secrets.yml
 # Key (this is the big one)
 sed -i "s/corvallis_or/${key}/g" *
 # Domain
 sed -i "s/corvallis-or\.proudcity\.com/${host}/g" *
 
-kubectl apply --namespace $NAMESPACE -f etc-kube/secrets.yml
-kubectl apply --namespace $NAMESPACE -f etc-kube/deployment.json
-kubectl apply --namespace $NAMESPACE -f etc-kube/service.json
-#kubectl create --namespace $NAMESPACE -f etc-kube/ingress-ssl.yml
-kubectl apply --namespace $NAMESPACE -f etc-kube/ingress.yml
 
+kubectl apply --namespace $NAMESPACE -f $dir/secrets.yml
+kubectl apply --namespace $NAMESPACE -f $dir/deployment.json
+kubectl apply --namespace $NAMESPACE -f $dir/service.json
+# kubectl create --namespace $NAMESPACE -f etc-kube/ingress-ssl.yml
+#kubectl apply --namespace $NAMESPACE -f ingress.yml
+
+echo 'done'
 # Clean up
-rm secrets.yml
+#rm secrets.yml
 #rm -r $key
