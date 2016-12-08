20ea6f31e0ea12ea2552b34e7060dd1d3b0ca7c7
Jeff Lyon <jeff@albatrossdigital.com>
2016-12-07 20:59:21 -0800

    Move private repo downloading into entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -31,16 +31,6 @@ RUN curl -o /tmp/composer.phar http://getcomposer.org/composer.phar \
   && mv /tmp/composer.phar /usr/local/bin/composer && chmod a+x /usr/local/bin/composer
 RUN composer install
 
-# Install Gravity Forms
-RUN git clone https://source.developers.google.com/p/proudcity-1184/r/gravityforms /app/wordpress/wp-content/plugins/gravityforms
-# @todo: `mv` doesn't work because of a Docker FS bug: https://github.com/docker/docker/issues/4570
-RUN cp -r /app/wordpress/wp-content/plugins/gravityforms/modules/* /app/wordpress/wp-content/plugins && rm -r /app/wordpress/wp-content/plugins/gravityforms/modules
-
-# Install other non-free plugins
-RUN git clone https://source.developers.google.com/p/proudcity-1184/r/e-signature /app/wordpress/wp-content/plugins/e-signature \
-	&& git clone https://source.developers.google.com/p/proudcity-1184/r/e-signature-business-add-ons /app/wordpress/wp-content/plugins/e-signature-business-add-ons \
-	&& git clone https://source.developers.google.com/p/proudcity-1184/r/auth0 /app/wordpress/wp-content/plugins/auth0
-
 #RUN curl -o /tmp/markdown.zip https://littoral.michelf.ca/code/php-markdown/php-markdown-extra-1.2.8.zip \
 #  	&& unzip /tmp/markdown.zip -d  /app/wordpress/wp-content/plugins \
 #  	&& mv  /app/wordpress/wp-content/plugins/PHP\ Markdown\ Extra\ 1.2.8/markdown.php  /app/wordpress/wp-content/plugins/ \
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,4 +1,44 @@
 #!/bin/bash
 set -e
 
+# Clone private repos
+if [[ $GOOGLE_GIT_TOKEN ]]; then
+
+  # Add gcloud 
+  echo "machine source.developers.google.com login jeff@proudcity.com password ${GOOGLE_GIT_TOKEN}" >> $HOME/.netrc
+
+  # Install Gravity Forms
+  echo "Adding Gravity Forms"
+  git clone https://source.developers.google.com/p/proudcity-1184/r/gravityforms /app/wordpress/wp-content/plugins/gravityforms
+  # @todo: `mv` doesn't work because of a Docker FS bug: https://github.com/docker/docker/issues/4570
+  cp -r /app/wordpress/wp-content/plugins/gravityforms/modules/* /app/wordpress/wp-content/plugins
+  rm -r /app/wordpress/wp-content/plugins/gravityforms/modules
+
+  # Install other non-free plugins
+  echo "Adding non-free plugins"
+  cd /app/wordpress/wp-content/plugins
+  git clone https://source.developers.google.com/p/proudcity-1184/r/auth0
+
+  # Add custom themes
+  if [[ $WORDPRESS_THEMES ]]; then
+    echo $WORDPRESS_THEMES > /tmp/themes
+    cd /app/wordpress/wp-content/plugins
+    while read s; do
+      git clone "${s}"
+      echo "Adding theme repo: ${s}"
+    done < /tmp/themes
+  fi
+
+  # Add custom plugins
+  if [[ $WORDPRESS_PLUGINS ]]; then
+    echo $WORDPRESS_PLUGINS > /tmp/plugins
+    cd /app/wordpress/wp-content/plugins
+    while read s; do
+      git clone "${s}"
+      echo "Adding plugin repo: ${s}"
+    done < /tmp/plugins
+  fi
+
+fi
+
 exec "$@"
