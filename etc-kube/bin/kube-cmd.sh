836e9083aceeb7bca1827025cd6c83a246709fd7
Jeff Lyon <jeff@albatrossdigital.com>
2016-12-09 00:33:00 -0800

    Moving around kube bin files

---
+++ etc-kube/bin/kube-cmd.sh
@@ -0,0 +1,17 @@
+# bash kube-cmd.sh $NAMESPACE beta wp "option get blogname"
+# bash kube-cmd.sh $NAMESPACE example bash "ls wordpress"
+
+namespace=$1
+app=${2}
+cmd=${3}
+
+if [ "$cmd" == "wp"* ]; then
+  fullcmd="$cmd --path=wordpress --allow-root"
+else
+  fullcmd=$cmd
+fi
+
+pod=$(kubectl --namespace=$namespace get pods --selector=app=$app --output=jsonpath={.items[0].metadata.name})
+
+echo "kubectl --namespace=$namespace exec $pod -- $fullcmd"
+kubectl --namespace=$namespace exec $pod -- $fullcmd $args
