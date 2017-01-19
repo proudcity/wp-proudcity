b263ecea6e22ccf5a92e820645cc0c448cdef347
Jeff Lyon <jeff@albatrossdigital.com>
2017-01-19 15:35:25 -0800

    Cleaning up paths in the initialize script

--- bin/initialize.sh
+++ bin/initialize.sh
@@ -3,19 +3,23 @@
 #bash initialize.sh $URL $ACCOUNT_EMAIL $CITY $STATE
 #bash initialize.sh www.cityofsanrafel.org
 #bash initialize.sh oakland-ca.proudcity.com  jeffrey.m.lyon@gmail.com Oakland CA
+
+thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 URL=${1}
 ACCOUNT_EMAIL=${2}
 CITY=${3}
 STATE=${4}
 STAGE=${4}
 
-bash ./change-url.sh "$URL"
+bash $thisdir/change-url.sh "$URL"
 
 cd /app/wordpress
 wp core update-db --allow-root
 wp option update proud_stage test --allow-root
 wp option delete google_analytics_key --allow-root
 wp user delete demo@proudcity.com --reassign=admin --allow-root
+echo "truncate table wp_rg_form_view;" | wp db cli --allow-root
 
 if  [[ !  -z  $ACCOUNT_EMAIL  ]]; then
   wp user create  $ACCOUNT_EMAIL $ACCOUNT_EMAIL --send-email=0 --role=editor --allow-root
