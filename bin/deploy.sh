5a33ba4b94558caba144f430b5d909ab0425caa3
Jeff Lyon <jeff@albatrossdigital.com>
2017-01-19 00:10:52 -0800

    Adding minify cache flush to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,7 @@ RELEASE=${1}
 cd wordpress
 bash /app/updates/release-${RELEASE}.sh
 wp core update-db --allow-root
+wp w3-total-cache flush minify --allow-root
 wp w3-total-cache flush --allow-root
 #wp w3-total-cache cdn_purge --allow-root
 wp rewrite flush --allow-root
