3daf868b83ac40c21a69bb6908e10282e1724874
Jeff Lyon <jeff@albatrossdigital.com>
2017-02-06 11:53:46 -0800

    Adding support for WP_REDIS_BACKEND_AUTH env var

--- www/wp-config.php
+++ www/wp-config.php
@@ -95,9 +95,9 @@ define('WPMS_SMTP_PASS', getenv('SMTP_PASS')); // SMTP authentication password, 
 // Varnish settings
 define('WP_CACHE', getenv("WP_CACHE"));
 define('WP_REDIS_BACKEND_HOST', getenv("WP_REDIS_BACKEND_HOST"));
+define('WP_REDIS_BACKEND_AUTH', getenv("WP_REDIS_BACKEND_AUTH"));
 define('WP_CACHE_KEY_SALT', getenv("HOST"));
 //WP_REDIS_BACKEND_PORT
-//WP_REDIS_BACKEND_AUTH
 //WP_REDIS_BACKEND_D
 //WP_REDIS_SERIALIZER
 
