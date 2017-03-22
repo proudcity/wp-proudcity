1a99ee0660d488d608bd52bc7fb1e0eb937daa73
Jeff Lyon <jeff@albatrossdigital.com>
2017-03-21 17:03:41 -0700

    Add wp w3-total-cache flush all

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,6 @@ RELEASE=${1}
 bash /app/updates/release-${RELEASE}.sh
 wp core update-db --allow-root
 wp w3-total-cache flush minify --allow-root
-wp w3-total-cache flush --allow-root
-#wp w3-total-cache cdn_purge --allow-root
+wp w3-total-cache flush all --allow-root
 wp rewrite flush --allow-root
 wp cache flush --allow-root
