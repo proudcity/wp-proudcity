d3b7652e08dbb508116c267f3182a7af4864149c
Jeff Lyon <jeff@albatrossdigital.com>
2017-03-22 15:02:45 -0700

    Disable Auth0 SSO https://github.com/proudcity/wp-proudcity/issues/932

---
+++ updates/release-1.27.1.sh
@@ -0,0 +1,14 @@
+# Updates for the 1.24.0 release
+# (no manual updates)
+
+PHP=${1}
+
+echo "Update auth0 welcome note"
+wp option get wp_auth0_settings --format=json --allow-root | php -r '
+$var = json_decode( fgets(STDIN) );
+$var->form_title = "Welcome to ProudCity";
+$var->icon_url = "https://my.proudcity.com/images/IconBlack.png";
+$var->sso = 0;
+$var->remember_users_session = true;
+print json_encode($var);
+' | wp option set wp_auth0_settings --format=json --allow-root
