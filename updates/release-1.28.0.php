bf303b07902b3118ada0bb078a725b6536539380
Jeff Lyon <jeff@albatrossdigital.com>
2017-03-30 19:11:19 -0700

    Creating script with manual commands for release 1.28.0

---
+++ updates/release-1.28.0.php
@@ -0,0 +1,4 @@
+<?php
+
+$config = new \W3TC\Generic_AdminActions_Config();
+$config->w3tc_config_reset();
---
+++ updates/release-1.28.0.sh
@@ -0,0 +1,7 @@
+# Updates for the 1.24.0 release
+# (no manual updates)
+
+PHP=${1}
+
+echo "Reset w3tc defauls"
+wp --allow-root eval-file $PHP
