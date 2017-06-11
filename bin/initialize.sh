2ad68b174308ee2212e64d1e3850a1e213226a13
Jeff Lyon <jeff@albatrossdigital.com>
2017-06-11 13:24:37 -0700

    Rm email from initialize script

--- bin/initialize.sh
+++ bin/initialize.sh
@@ -1,16 +1,15 @@
 # Scrubs and configures a site after cloning
 # Usage
-#bash initialize.sh $URL $WIPE_PRIVATE[true|false], $ACCOUNT_EMAIL $LOCATION $STAGE[test|live|example]
+#bash initialize.sh $URL $WIPE_PRIVATE[true|false] $LOCATION $STAGE[test|live|example]
 #bash initialize.sh false www.cityofsanrafel.org
-#bash initialize.sh true pueblo-co.proudcity.com  hsmith@pueblo.us "Pueblo/CO"
+#bash initialize.sh true pueblo-co.proudcity.com  "Pueblo/CO"
 
 thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 URL=${1}
 WIPE_PRIVATE=${2}
-ACCOUNT_EMAIL=${3}
-LOCATION=${4}
-STAGE=${5:test}
+LOCATION=${3}
+STAGE=${4:test}
 
 echo "-------------"
 echo "Running initialize.sh with parameters"
@@ -18,7 +17,7 @@ echo "URL: $URL"
 echo "ACCOUNT_EMAIL: $URL"
 echo "WIPE_PRIVATE: $WIPE_PRIVATE"
 echo "LOCATION: $URL"
-echo "STAGE: $URL"
+echo "STAGE: $STAGE"
 echo "-------------"
 
 bash $thisdir/change-url.sh "$URL"
@@ -30,10 +29,6 @@ if [ "$initialize" == "true" ]; then
     wp option delete google_analytics_key --allow-root
     wp user delete demo@proudcity.com --reassign=admin --allow-root
     echo "truncate table wp_rg_form_view;" | wp db cli --allow-root
-fi
-
-if  [[ !  -z  $ACCOUNT_EMAIL  ]]; then
-  wp user create  $ACCOUNT_EMAIL $ACCOUNT_EMAIL --send-email=0 --role=editor --allow-root
 fi
 
 # Get blogname, location, images, service center settings from ProudCity City API.
