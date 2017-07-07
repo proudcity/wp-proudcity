a3dbeb8bb4895d0bbeec0980f3ebe98291fe6a4f
Jeff Lyon <jeff@albatrossdigital.com>
2017-07-06 18:25:55 -0700

    Completing: Hotfix 1.34.2: Fix wp-stateless Body content media URL rewrite setting (wp-proudcity), July 6 2017 https://github.com/proudcity/wp-proudcity/issues/1038

--- updates/release-1.34.2.sh
+++ updates/release-1.34.2.sh
@@ -10,3 +10,12 @@ APP="${APP:-$WORDPRESS_DB_NAME}"
 echo "RUNNING COMMAND: wp search-replace ${URL}/wp-content/uploads https://storage.googleapis.com/proudcity/${APP}/uploads --allow-root"
 wp search-replace ${URL}/wp-content/uploads https://storage.googleapis.com/proudcity/${APP}/uploads --allow-root
 
+echo "Update option: sm"
+wp option get sm --allow-root
+
+
+wp option get sm --format=json --allow-root | php -r '
+$var = json_decode( fgets(STDIN) );
+$var->body_rewrite = "true";
+print json_encode($var);
+' | wp option set sm --format=json --allow-root
