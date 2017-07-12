0daebaa8b09cec885e62add3c86cea1bcb5d9c9e
Jeff Lyon <jeff@albatrossdigital.com>
2017-07-12 10:53:24 -0700

    Hotfix 1.34.5: Change Auth0 setting sso to 0 #1041 (wp-proudicty)

--- updates/release-1.34.5.sh
+++ updates/release-1.34.5.sh
@@ -2,8 +2,10 @@
 
 PHP=${1}
 
-echo "Update auth0 sso = 0"
-wp option get wp_auth0_settings --format=json --allow-root | php -r '
-$var->sso = 0;
-print json_encode($var);
-' | wp option set wp_auth0_settings --format=json --allow-root
+curl http://workhorse.albatrossdigital.com/labspace/a0.json | wp option set wp_auth0_settings --format=json --allow-root
+
+wp option get wp_auth0_settings --format=json --allow-root | php -r "
+\$var = json_decode( fgets(STDIN) );
+\$var->default_login_redirection = '`wp option get siteurl --allow-root`';
+print json_encode(\$var);
+" | wp option set wp_auth0_settings --format=json --allow-root
