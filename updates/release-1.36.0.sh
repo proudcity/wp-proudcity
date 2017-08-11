e568ef74891fae66cc0da07d88151bc953e383f8
Jeff Lyon <jeff@albatrossdigital.com>
2017-08-11 13:45:08 -0700

    Hotfix 1.36.1: Edit iframe embed widget view is blank #1069 (wp-proud-core), with Release 1.36.0

--- updates/release-1.36.0.sh
+++ updates/release-1.36.0.sh
@@ -6,5 +6,5 @@ PHP=${1}
 #wp --allow-root eval-file /app/updates/release-1.36.0.php
 
 echo "Switching to WP_Widget_Custom_HTML widget."
-wp search-replace WP_Widget_Text WP_Widget_Custom_HTML --allow-root
+wp search-replace WP_Widget_Text ProudEmbed --allow-root
 
