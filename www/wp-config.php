4bc84e1b925efb019041898c859addb9d4ad7f0d
Jeff Lyon <jeff@albatrossdigital.com>
2017-11-03 10:28:47 -0700

    Allow wordpress PROUD_AUTHENTICATION envvar value that requires a wordpress user account https://github.com/proudcity/proudcity-dashboard/issues/30

--- www/wp-config.php
+++ www/wp-config.php
@@ -161,10 +161,20 @@ if (getenv('TLS') === 'true'){
 
 
 
-// Do we need to put this behind an HTTP-auth wall?
-if (getenv("AUTH_REQUIRED") && php_sapi_name() !== "cli") {
-    $user = getenv("AUTH_USERNAME");
+// Do we need to put this behind an HTTP-auth wall, or redirect them to login to WordPress?
+$authRequired = getenv("AUTH_REQUIRED");
+if ($authRequired === 'wordpress') {
+
+  if (!is_user_logged_in() && !($_GET['auth0'] == 1 && !empty($_GET['code'])) ) {
+    header("Location: https://my.proudcity.com/login?msg=Please%20login%20to%20access%20your%20intranet");
+    exit;
+  }
+
+} elseif (getenv("AUTH_REQUIRED") && php_sapi_name() !== "cli") {
+
+  $user = getenv("AUTH_USERNAME");
     $pass = getenv("AUTH_PASSWORD");
+
 
     if ($user != $_SERVER['PHP_AUTH_USER'] || $pass != $_SERVER['PHP_AUTH_PW']) {
         header('WWW-Authenticate: Basic realm="ProudCity Realm"');
