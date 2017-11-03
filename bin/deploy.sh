6d535df57326f0e4252a0eafd020ed8ce2224977
Jeff Lyon <jeff@albatrossdigital.com>
2017-11-03 10:32:19 -0700

    Set proud_release on deploymentt #1179

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,10 @@
 RELEASE=${1}
 
 bash /app/updates/release-${RELEASE}.sh
+if [ -z "$RELEASE" ]; then
+  wp option update proud_release $RELEASE --allow-root
+fi
+
 wp core update-db --allow-root
 wp w3-total-cache flush minify --allow-root
 wp w3-total-cache flush all --allow-root
