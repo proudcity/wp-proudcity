bf2cc3b7f27667febdcef2fd68c4adfc6279ecb4
Jeff Lyon <jeff@albatrossdigital.com>
2019-06-26 22:40:25 -0700

    Fix, add replace for city-template.proudcity.com https://github.com/proudcity/wp-proudcity/issues/1513

--- bin/change-url.sh
+++ bin/change-url.sh
@@ -12,4 +12,6 @@ echo "CHANGING URL:"
 echo "OLD: $OLD_URL"
 echo "NEW: $URL"
 
-wp search-replace $(wp option get siteurl --allow-root)/wp-content/uploads/ https://storage.googleapis.com/proudcity/${STATELESS_MEDIA_DIRECTORY} --allow-root
+wp search-replace $OLD_URL $URL --skip-columns=guid --allow-root
+wp search-replace city-template.proudcity.com $HOST --skip-columns=guid --allow-root
+wp search-replace ${OLD_URL}/wp-content/uploads/ https://storage.googleapis.com/proudcity/${STATELESS_MEDIA_DIRECTORY} --skip-columns=guid --allow-root
