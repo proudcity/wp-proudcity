22d3eb4592f7635cede7167c815b8070689cad6b
Jeff Lyon <jeff@albatrossdigital.com>
2019-12-12 20:24:09 -0800

    Fixing migration to update google maps api key #1639

--- updates/release-1.83.0.sh
+++ updates/release-1.83.0.sh
@@ -2,6 +2,12 @@
 # (no manual updates)
 
 PHP=${1}
+KEY=$(wp option get google_api_key --allow-root)
+if [ "$KEY" = "AIzaSyCwfrA-_2rE-IiNf1z74xe1YeLolSeapnU" ]; then
+    echo "Updating google maps api key"
+    wp option update google_api_key AIzaSyASOGcX0_VY7W-Jucv01IdPOM-4Qml7ckA --allow-root
+else
+    echo "Strings are equal"
+fi
 
-echo "Update google maps api key"
-wp option update google_api_key AIzaSyASOGcX0_VY7W-Jucv01IdPOM-4Qml7ckA --allow-root
+
