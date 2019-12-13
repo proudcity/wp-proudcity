e5260d46861d8d9910ac7f65ce8d7790e37c3ffc
Jeff Lyon <jeff@albatrossdigital.com>
2019-12-12 18:58:04 -0800

    Creating script with manual commands for release 1.82.0

---
+++ updates/release-1.83.0.sh
@@ -0,0 +1,7 @@
+# Updates for the 1.83.0 release
+# (no manual updates)
+
+PHP=${1}
+
+echo "Update google maps api key"
+wp option update google_api_key AIzaSyASOGcX0_VY7W-Jucv01IdPOM-4Qml7ckA --allow-root
