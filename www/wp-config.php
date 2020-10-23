9e0955b85cb67338699ed31bb37a0941fbc28442
Jeff Lyon <jeff@albatrossdigital.com>
2020-10-22 22:00:45 -0700

    Platform: files being saved to the media library adds random numbres/letters to the front of the file name. #1754

--- www/wp-config.php
+++ www/wp-config.php
@@ -54,6 +54,7 @@ define( 'WP_STATELESS_MEDIA_MODE', 'ephemeral');
 define( 'WP_STATELESS_MEDIA_ROOT_DIR', getenv('STATELESS_MEDIA_DIRECTORY') . '/%date_year/date_month%/');
 define( 'WP_STATELESS_MEDIA_REWRITE_BODY_CONTENT_URL', 1);
 define( 'WP_STATELESS_COMPATIBILITY_GF', 1);
+define( 'WP_STATELESS_MEDIA_CACHE_BUSTING', false);
 
 // For ProudCity, to fix issues related to migration
 define( 'PROUD_WP_STATELESS_FORCE', getenv('PROUD_WP_STATELESS_FORCE'));
