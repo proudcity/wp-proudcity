c45b3175f96488fc316eb5f1dfa7e7f0776cdc8f
Jeff Lyon <jeff@albatrossdigital.com>
2021-03-11 22:06:19 -0800

    Creating script with manual commands for release 1.105.0

---
+++ updates/release-1.105.0.sh
@@ -0,0 +1,11 @@
+# Updates for the 1.96.0 release
+# Increase the cache-control settings in wp-stateless to improve site speed scores
+
+PHP=${1}
+
+# Increase cache lifetime in wp-stateless from 10 hrs to 30 days
+wp option get sm --format=json --allow-root | php -r '
+$var = json_decode( fgets(STDIN) );
+$var->body_rewrite = "false";
+print json_encode($var);
+' | wp option set sm --format=json --allow-root
