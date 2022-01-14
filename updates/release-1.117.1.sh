f336b17cd5da7763925f2fd8aaeb35b68622e7d2
Jeff Lyon <jeffrey.m.lyon@gmail.com>
2022-01-14 08:40:17 -0800

    Hotfix 1.117.1: Add classic-widgets plugin (wp-proudcity) #1943

---
+++ updates/release-1.117.1.sh
@@ -0,0 +1,7 @@
+# Updates for the 1.116.0 release
+# Uninstall Jquery Migrate (no longer needed for wp-fullcalendar)
+
+PHP=${1}
+
+# Uninstall Jquery Migrate (no longer needed for wp-fullcalendar)
+wp plugin activate classic-widgets --allow-root
