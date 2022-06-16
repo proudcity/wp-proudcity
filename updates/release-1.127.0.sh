f7318766792f12bc26138362328c93cadfee3481
Curtis McHale <curtis@curtismchale.ca>
2022-06-16 13:52:55 -0700

    updated comments to be accurate

--- updates/release-1.127.0.sh
+++ updates/release-1.127.0.sh
@@ -1,26 +1,26 @@
-// activating the media library plugin
+# activating the media library plugin
 wp activate plugin wp-media-folder --allow-root
 
-// deactivate and then uninstall W3TC
+# deactivate and then uninstall W3TC
 wp plugin deactivate w3-total-cache --uninstall --allow-root
 
-// activate wp rocket
+# activate wp rocket
 wp plugin activate wp-rocket --allow-root
 
-// activate wp rocket rest cache
+# activate wp rocket rest cache
 wp plugin activate wp-rocket-cache-rest-api --allow-root
 
-// import the wp-rocket .json config
+# import the wp-rocket .json config
 wp rocket import /app/updates/wp-rocket-base-2022-06-14.json --allow-root
 
-// library-calendar page exclusion
+# library-calendar page exclusion
 wp post update $(wp post list --post_type=page --pagename=library-calendar --format=ids --allow-root) --meta_input='{"_rocket_exclude_minify_js":"1","_rocket_exclude_defer_all_js":"1"}' --allow-root
 
-// events exclusion
+# events exclusion
 wp post update $(wp post list --post_type=page --pagename=events --format=ids --allow-root) --meta_input='{"_rocket_exclude_minify_js":"1","_rocket_exclude_defer_all_js":"1"}' --allow-root
 
-// calendar exclusion
+# calendar exclusion
 wp post update $(wp post list --post_type=page --pagename=calendar --format=ids --allow-root) --meta_input='{"_rocket_exclude_minify_js":"1","_rocket_exclude_defer_all_js":"1"}' --allow-root
 
-// events calendar exclusion
+# events calendar exclusion
 wp post update $(wp post list --post_type=page --pagename=events-calendar --format=ids --allow-root) --meta_input='{"_rocket_exclude_minify_js":"1","_rocket_exclude_defer_all_js":"1"}' --allow-root
