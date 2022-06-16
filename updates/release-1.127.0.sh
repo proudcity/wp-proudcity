83c32ad784150f5392798178b111b532a23ec57a
Curtis McHale <curtis@curtismchale.ca>
2022-06-16 15:43:43 -0700

    making sure we have the file argument

--- updates/release-1.127.0.sh
+++ updates/release-1.127.0.sh
@@ -11,7 +11,7 @@ wp plugin activate wp-rocket --allow-root
 wp plugin activate wp-rocket-cache-rest-api --allow-root
 
 # import the wp-rocket .json config
-wp rocket import /app/updates/wp-rocket-base-2022-06-14.json --allow-root
+wp rocket import --file=/app/updates/wp-rocket-base-2022-06-14.json --allow-root
 
 # library-calendar page exclusion
 wp post update $(wp post list --post_type=page --pagename=library-calendar --format=ids --allow-root) --meta_input='{"_rocket_exclude_minify_js":"1","_rocket_exclude_defer_all_js":"1"}' --allow-root
