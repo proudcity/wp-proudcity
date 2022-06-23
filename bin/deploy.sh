1b26020df6f94d1d2998262862757796dee00ce3
Jeff Lyon <jeffrey.m.lyon@gmail.com>
2022-06-23 10:31:29 -0700

    Remove calls to w3-total-cache during deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,7 +9,5 @@ echo "Clearing transient options in wp_options"
 wp --allow-root db query "DELETE from wp_options where option_name like '%transient%';"
 
 wp core update-db --allow-root
-wp w3-total-cache flush minify --allow-root
-wp w3-total-cache flush all --allow-root
 wp rewrite flush --allow-root
 wp cache flush --allow-root
