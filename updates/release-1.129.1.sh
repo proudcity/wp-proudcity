48259a01ed9b5361fd46a8b455acc58dcd5567a5
Jeff Lyon <jeffrey.m.lyon@gmail.com>
2022-07-18 09:14:26 -0700

    Add migration to re-enable plugins

---
+++ updates/release-1.129.1.sh
@@ -0,0 +1,4 @@
+# re-enable plugins after they didn't exist on instance boot
+wp plugin activate --allow-root gravityforms gravityformsstripe wp-media-folder wp-rocket wp-rocket-cache-rest-api
+
+# build trigger
