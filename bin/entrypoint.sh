231159097a4e138f477dc7002ff349842dd5981d
Jeff Lyon <jeffrey.m.lyon@gmail.com>
2022-09-07 21:47:48 -0700

    Get rid of redis session support

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -102,11 +102,12 @@ export PHP_MEMORY_LIMIT=${PHP_MEMORY_LIMIT:-"128M"}
 export UPLOAD_MAX_FILESIZE=${UPLOAD_MAX_FILESIZE:-"25M"}
 
 # Set up redis session configuration
-if [[ $REDIS_SESSION == "1" ]]; then
-  redisfile=/usr/local/etc/php/conf.d/redissessions.ini
-  echo "session.save_handler = redis" > $redisfile
-  echo "session.save_path = ${WORDPRESS_DB_NAME}redis:6379" >> $redisfile
-fi
+# Note: Sep 2022: we no longer do anything with REDIS
+# if [[ $REDIS_SESSION == "1" ]]; then
+#   redisfile=/usr/local/etc/php/conf.d/redissessions.ini
+#   echo "session.save_handler = redis" > $redisfile
+#   echo "session.save_path = ${WORDPRESS_DB_NAME}redis:6379" >> $redisfile
+# fi
 
 # Ensure that we have the latest wp-rocket con
 wp rocket import --allow-root /app/bin/wp-rocket.json
