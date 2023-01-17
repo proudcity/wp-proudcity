8ce29a72f80ea72abdf268f633d233850540794f
Curtis McHale <curtis@curtismchale.ca>
2023-01-17 11:36:58 -0800

    importing the wp rocket config file

--- updates/release-2023.01.17.sh
+++ updates/release-2023.01.17.sh
@@ -3,5 +3,8 @@
 # activating wp rocket
 wp plugin activate wp-rocket --allow-root
 
+# importing wp rocket
+wp rocket import --file=/app/updates/wp-rocket-base-2023-01-17.json --allow-root
+
 # clearing revisions
 wp post delete $(wp post list --post_type='revision' --format=ids --allow-root) --force --allow-root
