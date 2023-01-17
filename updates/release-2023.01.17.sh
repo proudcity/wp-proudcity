6f255d118315f88d50652c7caeb0ce842e38c7dc
Curtis McHale <curtis@curtismchale.ca>
2023-01-17 11:47:56 -0800

    callingc the proper file name

--- updates/release-2023.01.17.sh
+++ updates/release-2023.01.17.sh
@@ -4,7 +4,7 @@
 wp plugin activate wp-rocket --allow-root
 
 # importing wp rocket
-wp rocket import --file=/app/updates/wp-rocket-base-2023-01-17.json --allow-root
+wp rocket import --file=/app/updates/wp-rocket-2023-01-17.json --allow-root
 
 # clearing revisions
 wp post delete $(wp post list --post_type='revision' --format=ids --allow-root) --force --allow-root
