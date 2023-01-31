ef5d19b0d93e1d3048f2391a63715ca1d9b4a193
Curtis McHale <curtis@curtismchale.ca>
2023-01-31 10:52:27 -0800

    removed old post types order plugin and turned on new one as there seems to be a security issue with the old one and it is no longer updated

--- updates/release-2023.01.31.sh
+++ updates/release-2023.01.31.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# turning off old post types order plugin
+wp plugin deactivate intuitive-custom-post-order --allow-root
+
+# turning on new post type order plugin
+wp plugin activate post-types-order --allow-root
+
 # activating wp rocket
 wp plugin activate wp-rocket --allow-root
 
