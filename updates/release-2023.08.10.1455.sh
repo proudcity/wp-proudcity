fdc7ad9356a8dc9a4df9782450b19f21c69832f4
Curtis McHale <curtis@curtismchale.ca>
2023-08-10 15:21:31 -0700

    properly named file for rocket json

--- updates/release-2023.08.10.1455.sh
+++ updates/release-2023.08.10.1455.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # importing wp rocket
-wp rocket import --file=/app/updates/wp-rocket-2023-01-10.json --allow-root
+wp rocket import --file=/app/updates/wp-rocket-2023-08-10.json --allow-root
 
