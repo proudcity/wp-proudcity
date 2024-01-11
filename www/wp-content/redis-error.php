f9fd3d6e36e759623e12ae350fe5ab902e866590
Curtis McHale <curtis@curtismchale.ca>
2024-01-11 07:43:24 -0800

    adding js countdown to page and forcing refresh

--- www/wp-content/redis-error.php
+++ www/wp-content/redis-error.php
@@ -4,4 +4,15 @@
 
 <p>Refresh your browser and this should resolve. If the problem persists please get in touch with support.</p>
 
+<p id="countdown"></p>
+
+<script type="text/javascript">
+(function countdown(remaining) {
+    if(remaining <= 0)
+        location.reload(true);
+    document.getElementById('countdown').innerHTML = remaining;
+    setTimeout(function(){ countdown(remaining - 1); }, 1000);
+})(5); // 5 seconds
+</script>
+
 
