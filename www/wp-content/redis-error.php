5d1dfd5b2b7c1e411db57038fe10d39fe6ad02aa
Curtis McHale <curtis@curtismchale.ca>
2024-01-11 07:03:00 -0800

    testing custom redis error page

---
+++ www/wp-content/redis-error.php
@@ -0,0 +1,7 @@
+<h2>An error was encountered</h2>
+
+<p>Looks like we've encountered a minor hiccup.</p>
+
+<p>Refresh your browser and this should resolve. If the problem persists please get in touch with support.</p>
+
+
