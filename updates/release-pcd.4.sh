28e6cf342f0936b862920d9841253e963f1d36d7
Curtis McHale <curtis@curtismchale.ca>
2024-05-22 14:42:11 -0700

    adding the widget layout to user settings

--- updates/release-pcd.4.sh
+++ updates/release-pcd.4.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# activate our new plugin
 wp plugin activate wp-proud-admin-theme --allow-root
+
+# set widget order
+wp user meta update '{
+  "normal": "proud_news_feed,proud_training,proud_help_center,proud_support",
+  "side": "proud_clean_up,proud_plain_language,proud_review",
+  "column3": "",
+  "column4": ""
+}' --format=json --allow-root
