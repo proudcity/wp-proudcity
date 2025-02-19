c4489cd1195b987c54fed5108cba7df585a9621d
Curtis McHale <curtis@curtismchale.ca>
2025-02-19 09:30:28 -0800

    - preloading cache files
    Given issues like [PCD108](https://github.com/proudcity/pc-dev-issues/issues/108#issuecomment-2666977511) where we see sites without CSS loaded I wonder if the database is thinking it has the cache files but we're on a new pod that just spun up and thus doesn't have the cache files in the location on the server. The server files are pod specific whereas the database is shared amongst pods behind REDIS.
    
    This preloads the cache files when we spin up a pod.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -140,6 +140,9 @@ export UPLOAD_MAX_FILESIZE=${UPLOAD_MAX_FILESIZE:-"25M"}
 echo "Importing WP Rocket configuration"
 wp rocket import --allow-root /app/bin/wp-rocket.json
 
+# preload cache files
+wp rocket preload --allow-root
+
 # enable redis requires the Redis Cache plugin to be active
 echo "Enabling Redis"
 wp redis enable --allow-root
