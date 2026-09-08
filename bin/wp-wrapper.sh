e784772dd7bba440614c96dd075266382155efe6
Curtis McHale <curtis@curtismchale.ca>
2026-09-08 11:57:33 -0700

    - fixing wrapper.sh by skipping its
    Issue: https://github.com/proudcity/wp-proudcity/issues/2921

--- bin/wp-wrapper.sh
+++ bin/wp-wrapper.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 # This is a wrapper so that wp-cli can run as the www-data user so that permissions
-# remain correct
-wp $*
+# remain correct.
+#
+# Arguments are forwarded with "$@", not $*. Unquoted $* re-splits every
+# argument on whitespace and lets the shell glob what is left, which made
+# `wp eval`, `wp db query` and any --field="some value" unusable through this
+# wrapper (proudcity/wp-proudcity#2921). Callers must pass each wp-cli
+# argument separately; a whole command handed over as one string will now
+# reach wp as a single argv entry.
+exec wp "$@"
